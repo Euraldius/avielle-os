ad30b102ceff24dcaf29bfcfc50f300a625de77b
Avielle Wolfe <aviellewolfe@gmail.com>
2018-11-01 21:08:26 -0400

    Move HomeDirectory to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,37 @@
 <template>
   <div class="home">
-    <HomeDirectory />
+    <div class="home-directory-container">
+      <div class="home-directory">
+        <router-link to="/resume" class="directory-link">resume</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HomeDirectory from '@/components/HomeDirectory.vue';
-
 export default {
   name: 'home',
-  components: {
-    HomeDirectory,
-  },
 };
 </script>
 
-<style>
-.home {
-  background-color: black;
-  height: 100vh;
+<style lang="scss">
+$fuschia: #FF00FF;
+$text-white: #d1d0cf;
+
+.directory-link {
+  color: $text-white;
+}
+
+.home-directory-container {
+  padding: 100px;
+}
+
+.home-directory {
+  background-color: rgba($fuschia, 0.2);
+  border: $fuschia 1px solid;
+  box-shadow: 10px -10px 5px 5px rgba($fuschia, 0.2);
+  border-radius: 3px;
+  height: 500px;
+  padding: 50px;
 }
 </style>
