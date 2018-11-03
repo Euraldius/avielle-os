9b70301110b32144933ef71dcd1c21c491b19bd2
Avielle Wolfe <aviellewolfe@gmail.com>
2018-11-03 13:45:59 -0400

    Create Finder

---
+++ src/components/Finder.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="finder-container">
+    <div class="finder">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'finder',
+};
+</script>
+
+<style lang="scss">
+$fuschia: #FF00FF;
+
+.finder-container {
+  padding: 100px;
+}
+
+.finder {
+  background-color: rgba($fuschia, 0.2);
+  border: $fuschia 1px solid;
+  box-shadow: 10px -10px 5px 5px rgba($fuschia, 0.2);
+  border-radius: 3px;
+  height: 500px;
+  padding: 50px;
+}
+</style>
