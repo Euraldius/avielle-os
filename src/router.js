51e49117796546e7474e1210500b4d8fabc24a8f
Avielle Wolfe <aviellewolfe@gmail.com>
2018-11-06 16:46:24 -0500

    Home -> Resume

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/views/Home.vue';
 import StartupScreen from '@/views/StartupScreen.vue';
 import Resume from '@/views/Resume.vue';
 
@@ -16,11 +15,6 @@ export default new Router({
     {
       path: '/home',
       name: 'home',
-      component: Home,
-    },
-    {
-      path: '/resume',
-      name: 'resume',
       component: Resume,
     },
   ],
