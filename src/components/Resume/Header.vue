7a4cf5f3f228be6791d06aefc06feb51f7df987a
Avielle Wolfe <aviellewolfe@gmail.com>
2018-11-08 10:41:44 -0500

    Extract resume header

---
+++ src/components/Resume/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <section>
+      <h1>Hi! I'm <strong>Avielle Wolfe</strong></h1>
+      <p><a href="mailto:aviellewolfe@pm.me">aviellewolfe@pm.me</a></p>
+    </section>
+    <section>
+      <p>
+        I've spent <strong>{{ timeAsDeveloper }}</strong> working as a web developer.
+      </p>
+      <p>
+        I've worked for <strong>3</strong> employers.<br/>
+        <strong>2</strong> of those are consultancies.<br/>
+        Between the two, I've worked with <strong>15</strong> clients.
+      </p>
+      <p>
+        I'm practiced in <strong>Agile development</strong>,
+        <strong>TDD</strong>,
+        and <strong>XP</strong>.<br/>
+        I believe in applying them pragmatically.<br/>
+        Above all, I believe in the Agile mantra:<br/>
+        <em>People over process!</em>
+      </p>
+      <p>
+        I believe in making applications by considering user input,<br/>
+        By protecting users' <em>security</em> and <em>privacy</em>,<br/>
+        And by keeping our technology choices simple for the sake of future developers.
+      </p>
+    </section>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'resume-header',
+};
+</script>
--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -2,33 +2,7 @@
   <finder>
     <div class="resume">
       <info-box classes="header">
-        <section>
-          <h1>Hi! I'm <strong>Avielle Wolfe</strong></h1>
-          <p><a href="mailto:aviellewolfe@pm.me">aviellewolfe@pm.me</a></p>
-        </section>
-        <section>
-          <p>
-            I've spent <strong>{{ timeAsDeveloper }}</strong> working as a web developer.
-          </p>
-          <p>
-            I've worked for <strong>3</strong> employers.<br/>
-            <strong>2</strong> of those are consultancies.<br/>
-            Between the two, I've worked with <strong>15</strong> clients.
-          </p>
-          <p>
-            I'm practiced in <strong>Agile development</strong>,
-            <strong>TDD</strong>,
-            and <strong>XP</strong>.<br/>
-            I believe in applying them pragmatically.<br/>
-            Above all, I believe in the Agile mantra:<br/>
-            <em>People over process!</em>
-          </p>
-          <p>
-            I believe in making applications by considering user input,<br/>
-            By protecting users' <em>security</em> and <em>privacy</em>,<br/>
-            And by keeping our technology choices simple for the sake of future developers.
-          </p>
-        </section>
+        <resume-header />
       </info-box>
       <info-box classes="stats">
         <section>
@@ -114,12 +88,14 @@
 import moment from 'moment';
 import Finder from '@/components/Finder.vue';
 import InfoBox from '@/components/InfoBox.vue';
+import ResumeHeader from '@/components/Resume/Header.vue';
 
 export default {
   name: 'resume',
   components: {
     Finder,
     InfoBox,
+    ResumeHeader,
   },
   computed: {
     timeAsDeveloper: function timeAsDeveloper() {
