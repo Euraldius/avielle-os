5cf626fcfa6153ea6cd0910be72576dbeac1c240
Avielle Wolfe <aviellewolfe@gmail.com>
2018-12-13 20:35:58 -0500

    Fix base url for custom domain

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '/avielle-os/' : '/',
+  // future Avi: if you remove your custom domain from GH pages, change the
+  // production path of this ternary to `/avielle-os/`
+  baseUrl: process.env.NODE_ENV === 'production' ? '/' : '/',
 };
